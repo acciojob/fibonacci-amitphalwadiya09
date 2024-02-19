b70ff88be3c9da80707a00218ec6cd7b961c0d30
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-19 11:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function fibonacci(num) {
-	if (num <= 0) {
-        return "Invalid input. Please provide a positive integer greater than 0.";
-    }
-
-    let a = 0;
-    let b = 1;
-
-    for (let i = 2; i < num; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-
-    return b;
+	if(num==1)
+	{
+		return 0;
+	}
+	else if(num==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibonacci(num-1)+fibonacci(num-2)
+	}
+	
+	
 }
 
 module.exports = fibonacci;
