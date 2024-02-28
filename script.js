770829b31037649ab6a8b80e9e902c25b0d19be6
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-28 23:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function fibonacci(num) {
 	if(num===0)
 	{
-		return ;
+		return 0;
 	}
-	if (num === 1) 
+	if (num === 1 || num === 2) 
 	{
-        return 0;
-    } else if (num === 2) {
         return 1;
-    }
-    } else {
+    } 
+	else 
+	{
         return fibonacci(num - 1) + fibonacci(num - 2);
     }
 	
